eee382b1378491aa0c0c7ff4875be80eac376e67
Aleksey Moseev <a12345678@Alekseis-MacBook-Pro.local>
2024-09-27 13:24:59 +0300

    fix for task

--- vowel_hash.rb
+++ vowel_hash.rb
@@ -2,6 +2,6 @@
 
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 
-result_hash = (('a'..'z').map.each_with_index { |letter, index| [letter, index + 1] if vowels.include? letter }).compact
+result_hash = (('a'..'z').filter_map.each_with_index { |letter, index| [letter, index + 1] if vowels.include? letter })
 
 print "Result Hash: #{result_hash}"
